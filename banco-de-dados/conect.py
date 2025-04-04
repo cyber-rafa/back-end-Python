e4a4f1ed3d28b5f5d1c5f74efc8ed901cca79add
cyber-rafa <tvmarte17@gmail.com>
2025-04-04 19:24:55 -0300

    up

--- banco-de-dados/conect.py
+++ banco-de-dados/conect.py
@@ -6,6 +6,7 @@ ROOT_PATH = Path(__file__).parent
 
 con = sqlite3.connect(ROOT_PATH / 'banco.sqlite')
 cursor = con.cursor()
+cursor.row_factory = sqlite3.Row
 
 def criar_tabela(cursor):
     cursor.execute(
@@ -30,4 +31,25 @@ def deletar_registro(con, cursor, id):
         'DELETE FROM clientes WHERE id=?;', data)
     con.commit()
 
-deletar_registro(con, cursor,  1)
+def inserir_muitos(con, cursor, dados):
+    cursor.executemany('INSERT INTO clientes (nome, email) VALUES (?, ?)', dados)
+    con.commit()
+
+#dados = [
+    #('rafa', 'rafa@gmail'),
+    #('thais', 'thais@gmail'),
+    #('pedro', 'pedro@gmail'),
+#]
+
+
+def recuperar_clentes(cursor, id):
+    cursor.execute('SELECT * FROM clientes WHERE id=?', (id,))
+    return cursor.fetchone()
+
+def list_clentes(cursor):
+    return cursor.execute('SELECT * FROM clientes ORDER BY nome')
+
+clientes = list_clentes(cursor)
+for cliente in clientes:
+    print(dict(cliente))
+    print(cliente['id'], cliente['nome'], cliente['email'])
