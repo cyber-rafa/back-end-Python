132c6840390e9fcb4764b22d0814467303371b8b
cyber-rafa <tvmarte17@gmail.com>
2025-04-20 02:36:37 +0100

    up

--- Desenvolvendo APIs com Flask/src/app.py
+++ Desenvolvendo APIs com Flask/src/app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+@app.route('/olamundo/<usuario>/<int:idade>')
+def hello_word(usuario, idade):
+    print(idade)
+    print(f'tipo de variavel idade: {type(idade)}')
+    print(f'usuario: { type(usuario)}')
+    return f'<h1>Ola mundo! usuario: {usuario}</h1>'
 
 @app.route("/admin")
 def admin():
